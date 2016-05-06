4d9fbf49aa33596bd798addd695a496d49e3e2c8
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2016-05-06 14:42:08 -0400

    Ajout des constructeurs de la classe media

--- codeSource/medias/Media.java
+++ codeSource/medias/Media.java
@@ -4,5 +4,17 @@ public abstract class Media{
 	protected int mYear;
 	protected String mTitle;
 	
+	public Media(String pFileName,int pDuration,int pYear,String pTitle){
+		mFileName = pFileName;
+		mDuration = pDuration;
+		mYear = pYear;
+		mTitle = pTitle;
+	}
+	
+	public Media(Media pMedia){
+		mFileName = pMedia.mFileName;
+		mDuration = pMedia.mDuration;
+		mYear = pMedia.mYear;
+		mTitle = pMedia.mTitle;
+	}
 }
-t 
