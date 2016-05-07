2bb8969071d43c3d92e327f61c30e0aa298a4c2b
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2016-05-06 22:15:01 -0400

    Add MediaAudio constructors

--- codeSource/medias/MediaAudio.java
+++ codeSource/medias/MediaAudio.java
@@ -1,4 +1,16 @@
-public class MediaAudio extends Media{
+public abstract class MediaAudio extends Media{
 	  private String mArtist;
-	  private int mAlbum;
+	  private String mAlbum;
+	  
+	  public MediaAudio(String pFileName,int pDuration,int pYear,String pTitle,String pArtist,String pAlbum){
+		  super(pFileName,pDuration,pYear,pTitle);
+		  mArtist = pArtist;
+		  mAlbum = pAlbum;
+	  }
+	  
+	  public MediaAudio(MediaAudio pMedia){
+		  super(pMedia);
+		  this.mArtist = pMedia.mArtist;
+		  this.mAlbum = pMedia.mAlbum;
+	  }
 }
