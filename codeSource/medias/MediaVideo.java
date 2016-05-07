c7a147ae271a1d0478de87dd0e722314e6539fc3
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2016-05-06 22:16:43 -0400

    Add MediaVideo constructors

--- codeSource/medias/MediaVideo.java
+++ codeSource/medias/MediaVideo.java
@@ -1,5 +1,20 @@
-public class MediaVideo extends Media{
+public  class MediaVideo extends Media{
 	private String mFrameRate;
 	private int mWidth;
 	private int mHeight;
+	
+	public MediaVideo(String pFileName,int pDuration,int pYear,String pTitle,String pFrameRate,int pWidth,int pHeight){
+		super(pFileName,pDuration,pYear,pTitle);
+		mFrameRate = pFrameRate;
+		mWidth = pWidth;
+		mHeight = pHeight;
+	}
+	
+	public MediaVideo(MediaVideo pMedia){
+		  super(pMedia);
+		 this.mFrameRate = pMedia.mFrameRate;
+		 this.mWidth = pMedia.mWidth;
+		 this.mHeight = pMedia.mHeight;
+	  }
+	
 }
