147c5f52a75c18b61004214803e08c7a7e5d54cb
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2016-05-08 11:25:10 -0400

    Add Vector methods

--- codeSource/medias/MediaLibrary.java
+++ codeSource/medias/MediaLibrary.java
@@ -1,2 +1,25 @@
+import java.util.*;
 public class MediaLibrary{
+	public final int VIDE = 0;
+	Vector<Media> mMediaList;
+	
+	public MediaLibrary(){
+		mMediaList = new Vector<>(VIDE) ;
+	}
+	
+	public void remove(int pIndex){
+		mMediaList.remove(pIndex);
+	}
+	
+	public void add(Media pMedia){
+		mMediaList.addElement(pMedia);
+	}
+	
+	public int size(){
+		return mMediaList.size();
+	}
+	
+	public Media get(int pIdx){
+		return mMediaList.get(pIdx);
+	}
 }
