9152701e5383f3595810cbf6f7c473f3d82d206b
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2016-05-11 18:58:47 -0400

    change package to medias / implement MediaConstant interface into MediaAudio

--- codeSource/medias/MediaAudio.java
+++ codeSource/medias/MediaAudio.java
@@ -1,6 +1,8 @@
+package medias;
 import java.io.*;
 import java.util.*;
-public  class MediaAudio extends Media{
+
+public  class MediaAudio extends Media implements MediaConstant{
 	  private int MP3=1;
 	  private int MP4=0;
 	  private String mArtist;
@@ -19,18 +21,22 @@ public  class MediaAudio extends Media{
 	  }
 	  
 	  public MediaAudio(Scanner pScan){
-
-		int mediaType=-1;
-		String filePath;
-	    mediaType = Integer.valueOf(pScan.next());
-		System.out.println("mediaType"+mediaType);
-		if(mediaType == MP3){
-			System.out.println(pScan.next());
-			filePath = pScan.next();
-			System.out.println(filePath);
-	    }
-			
-	}
+           //int typeMedia;
+		      // typeMedia = Integer.valueOf(pScan.nextLine());
+			   pScan.nextLine();
+			   mFileName = pScan.nextLine();
+			   pScan.nextLine();
+			   mDuration = Integer.valueOf(pScan.nextLine());
+			   pScan.nextLine();
+			   mYear = Integer.valueOf(pScan.nextLine());
+			   pScan.nextLine();
+			   mTitle = pScan.nextLine();
+			   pScan.nextLine();
+			   mArtist = pScan.nextLine();
+			   pScan.nextLine();
+			   mAlbum = pScan.nextLine();
+			   
+	  }
 	
 	public String[] getFullArrayInfo(){
 		return null;
@@ -38,18 +44,21 @@ public  class MediaAudio extends Media{
 	
 	@Override
 	 public String toString(){
-		return mArtist+" "+mAlbum+"";
+		return super.toString()+"\nARTIST\n"+mArtist+"\nALBUM\n"+mAlbum;
 	}
 	
-	public static void main(String[] args) throws IOException{
+	//Main de debeugage
+	/*public static void main(String[] args) throws IOException{
 		Scanner pScan=null;
-			 pScan = new Scanner(new File("libLocal.txt"));
+			  pScan = new Scanner(new File("../libLocal.txt"));
 			 if(pScan == null){
-				 throw new IOException("Fichier de la library local introuvablr");
+				 throw new IOException("Fichier de la library local introuvable");
 			 }
-			 MediaAudio ma = new MediaAudio(pScan);	
-			 System.out.println(ma);
+			 Media ma = new MediaAudio("fichier.mp3",3,2000,"Heavy dent","Tech n9ne","unkown");	
+			 Media ma2 = new MediaAudio(pScan);
+			 //System.out.println(ma);
+			 System.out.println(ma2);
 
-	}
+	}*/
 	  
 }
