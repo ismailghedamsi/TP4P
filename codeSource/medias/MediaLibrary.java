d762ca17aa48eed504fb3c3538b13d78491b9a87
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2016-05-22 19:48:21 -0400

    Complete MediaLibrary methods

--- codeSource/medias/MediaLibrary.java
+++ codeSource/medias/MediaLibrary.java
@@ -3,10 +3,14 @@ import java.util.*;
 import java.io.*;
 public class MediaLibrary{
 	public final int VIDE = 0;
-	Vector<Media> mMediaList;
+	private Vector<Media> mMediaList;
 	
 	public MediaLibrary(){
 		mMediaList = new Vector<>(VIDE) ;
+	}
+	
+	public Vector<Media> getMediaList(){
+		return mMediaList;
 	}
 	
 	public MediaLibrary(Scanner pScan){
@@ -58,6 +62,38 @@ public class MediaLibrary{
 	
 	public String[] getVideoFieldInfo(){
 		String[] videoField = {"Titre","Annee","Duree","Frequance de trame","Dimention","ficher"};
+		return videoField;
+	}
+	
+	public String[][] getBasicLibraryInfo(){
+		 String[][] basicLibraryInfo = new String[mMediaList.size()][3];
+		 
+		for(int i=0;i<mMediaList.size();i++){
+			basicLibraryInfo[i][MediaConstant.TITLE] = mMediaList.get(i).getTitle();
+			basicLibraryInfo[i][MediaConstant.YEAR] = mMediaList.get(i).getYear()+"";
+			basicLibraryInfo[i][MediaConstant.DURATION] = mMediaList.get(i).getDuration()+"";
+			
+		}
+		return basicLibraryInfo;
+	}
+	
+	public String[] getFullMediaInfo(int pIdx){
+		String[] fullMediaInfo = new String[6];
+		fullMediaInfo[MediaConstant.TITLE]=mMediaList.get(pIdx).getTitle();
+		fullMediaInfo[MediaConstant.YEAR]=mMediaList.get(pIdx).getYear()+"";
+		fullMediaInfo[MediaConstant.DURATION]=mMediaList.get(pIdx).getDuration()+"";
+		if(mMediaList.get(pIdx) instanceof MediaVideo){
+			MediaVideo mediaV = (MediaVideo)mMediaList.get(pIdx);
+			fullMediaInfo[MediaConstant.FRAME_RATE] = mediaV.getFrameRate()+"";
+			fullMediaInfo[MediaConstant.DIMENSION] = mediaV.getWidth()+"X"+mediaV.getHeight();
+			
+		}else if(mMediaList.get(pIdx) instanceof MediaAudio){
+			MediaAudio mediaA = (MediaAudio)mMediaList.get(pIdx);
+			fullMediaInfo[MediaConstant.ARTIST] = mediaA.getArtist();
+			fullMediaInfo[MediaConstant.ALBUM] = mediaA.getAlbum();
+		}
+		fullMediaInfo[MediaConstant.FILE_NAME] = mMediaList.get(pIdx).getFileName();
+		return fullMediaInfo;
 	}
 	
 	public static void main(String[] args) throws IOException{
@@ -67,8 +103,16 @@ public class MediaLibrary{
 				 throw new IOException("Fichier de la library local introuvable");
 			 }
 			 MediaLibrary ml1 = new MediaLibrary(pScan);
+			 System.out.println("Taille vecteur "+ ml1.mMediaList.size());
 			  for(Media currentMedia : ml1.mMediaList){
 				  System.out.println(currentMedia);
+			  }
+			  
+			   String[][] info = ml1.getBasicLibraryInfo();
+			  for(int i=0;i<info.length;i++){
+				  for(int j=0;j<info[i].length;j++){
+					  System.out.println("info[i][j] "+info[i][j]);
+				  }
 			  }
 			  
 	}
